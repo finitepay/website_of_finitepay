fb46502a47241be58d319818d74123aec069d43a
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-24 15:46:20 +0300

    feat: implement Heros component with dynamic data rendering and enhance card styles

---
+++ src/Data/herosdata.js
@@ -0,0 +1,24 @@
+import apple from '../images/icons8-app-store-48.png'
+import google from '../images/icons8-play-store-48.png'
+import star from '../images/icons8-star-48.png'
+
+const connectdata = [
+    {
+        id: 1,
+        icons : apple,
+        alt: 'apple',
+        view: "4.8k",
+        stars : star,
+        Text: "on App Store 3.5K Reviews"
+    },
+    {
+        id: 2,
+        icons : google,
+        alt: 'google',
+        view: "4.8k",
+        stars : star,
+        Text: "on Google Play 6.8K Reviews"
+    }
+];
+
+export default connectdata;
--- src/Pages/Heros.jsx
+++ src/Pages/Heros.jsx
@@ -1,3 +1,5 @@
+import connectdata from "../Data/herosdata"
+
 
 export default function Heros() {
   return (
@@ -14,10 +16,30 @@ export default function Heros() {
          </div>
        </div> 
        
-       {/* Hero Image  and client details*/}
-       <div className="">
-        <img src="" alt="" />
-       </div>
+       {/* Hero details  and client details*/}
+      <div className="heros-details-content">
+        {/* First container design */}
+        <div className="first-container">
+            <div className="heros-card-container">
+            {
+                connectdata.map((heros) => (
+                  <div key={heros.id} className="heros-card">
+                    <div className="heros-card-image">
+                      <img src={heros.icons} alt={heros.alt} />
+                    </div>
+                    <div className="heros-card-details">
+                    <div className="heros-card-image-two">
+                      <img src={heros.stars} alt={heros.alt} />
+                    </div>
+                      <h2>{heros.view}</h2>
+                      <p>{heros.Text}</p>
+                    </div>
+                  </div>
+                ))
+            }
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
