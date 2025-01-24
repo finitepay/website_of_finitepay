5ac5992aa7b5dd7803780881bc989cb9e976b99c
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-24 15:03:23 +0300

    feat: enhance Heros component with action buttons and improve layout styles

--- src/Pages/Heros.jsx
+++ src/Pages/Heros.jsx
@@ -2,11 +2,16 @@
 export default function Heros() {
   return (
     <div className="herosBody">
-       <div>
+       <div className="heros-main-content">
          <div className="">
             <h1 className="header-text">FinitePay</h1>
          </div>
-         <div className=""></div>
+
+         {/* Call to action button */}
+         <div className="button-for-action">
+              <button className="button">Personal Account</button>
+              <button className="button">Business Account</button>
+         </div>
        </div>    
     </div>
   )
