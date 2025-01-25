74c457b66b00992e92db7a794e2e3bff4e6fd71f
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-25 12:58:36 +0300

    feat: implement happy clients slider carousel with dynamic content and styling updates

--- src/Pages/Slidercarousel.jsx
+++ src/Pages/Slidercarousel.jsx
@@ -1,9 +1,28 @@
+import { happyClient } from "../Data/herosdata";
 
 
 export default function Slidercarousel() {
   return (
     <div className="slider-happy-clients">
-        
+        <div className="slider-happy-clients-content">
+            {
+                happyClient.map((happyClient) => (
+                    <div key={happyClient.id} className="slider-happy-clients-card">
+                      <div className="slider-happy-clients-card-image">
+                         <img src={happyClient.image} alt={happyClient.alt} />
+                      </div>
+                        <div className="">
+                        <h2>{happyClient.name}</h2>
+                        <p>{happyClient.Location}</p>
+
+                       <div className="">
+                          <p>{happyClient.Text}</p>
+                       </div>
+                    </div>
+                    </div>
+                ))
+            }
+        </div>
     </div>
   )
 }
